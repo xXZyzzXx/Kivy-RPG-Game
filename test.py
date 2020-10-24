7f4b39dd8592d207bfbf6e5e360f83ab736aa327
xXZyzzXx <officialtopdoge@gmail.com>
2020-10-25 02:06:44 +0300

    working prototipe

--- test.py
+++ test.py
@@ -5,25 +5,22 @@ w = 10
 
 A = [[col for col in range(w + 1)] for row in range(h)]
 map_index = np.array(A)
-#print(map_index)
+# print(map_index)
 
-passability = [[1 for j in range(w + 1)] for i in range(h)]
-pm = np.array(passability)
-#print(pm)
+pm = [[1.00 for j in range(w)] for i in range(h)]
 
-movement_map = [[-1 for k in range(w + 1)] for g in range(h)]
-rm = movement_map  #np.array(movement_map)
+rm = [[-1.00 for k in range(w)] for g in range(h)]
 
 
 def generate_move_map(x, y, moves=3):
     pm_width = 10
     pm_height = 10
-    if rm[x][y] < moves:
-        rm[x][y] = moves
-        for _x in range(-1, 1):  # Проходит циклом вокруг центра
-            for _y in range(-1, 1):
-                print(_x, _y)
-                if _x == 0 and _y == 0:
+    if rm[x][y] < float(moves):
+        rm[x][y] = float(moves)
+        for _x in range(-1, 2):  # Проходит циклом вокруг центра
+            for _y in range(-1, 2):
+                print(f'_x, _y: {_x, _y}')
+                if (_x, _y) != (0, 0):
                     nX = _x + x
                     nY = _y + y
                     print(f'nx,ny={nX, nY}, | _y, _x{_y, _x} | method {x, y, moves}')
@@ -32,7 +29,7 @@ def generate_move_map(x, y, moves=3):
                             move_weight = 1.5
                         else:
                             move_weight = 1
-                        t = moves - move_weight  # * pm[nX, nY]
+                        t = moves - move_weight * pm[nX][nY]
                         print(f'T={t}')
                         if t >= 0:
                             print(f'generating new coords: {nX, nY, t}')
@@ -40,28 +37,8 @@ def generate_move_map(x, y, moves=3):
                 _y += 1
             _x += 1
 
+
 generate_move_map(5, 5)
+
 for f in rm:
-   print(f)
-'''
-void GenMoveMap(int x, int y, double _points)
-            {
-                if (rm[x, y].Range < _points)
-                {
-                    rm[x, y].Range = _points;
-                    for (int _x = -1; _x <= 1; _x++)
-                        for (int _y = -1; _y <= 1; _y++)
-                            if (!(_x == 0 && _y == 0))
-                            {
-                                int nX = _x + x;
-                                int nY = _y + y;
-                                if (nX >= 0 && nY >= 0 && nX < pm.Width && nY < pm.Hight)
-                                {
-                                    double t =
-                                        _points - ((Math.Abs(_x) + Math.Abs(_y) == 2 ? 1.5 : 1)
-                                        * pm[nX, nY].Penalty);
-                                    if (t >= 0)
-                                        GenMoveMap(nX, nY, t);
-                                }
-                            }
-                }'''
+    print(f)
